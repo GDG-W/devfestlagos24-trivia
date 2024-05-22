c61eac772eaaf636d3603bd121977605af60468e
Jude Tochy <tochyokoye@gmail.com>
2024-05-22 21:04:07 +0100

    worked on project structure, landing page and leaderboard

---
+++ src/components/Leaderboard/Index.tsx
@@ -0,0 +1,72 @@
+import { CupIcon, UserIcon } from "../Icons/Leaderboard";
+import { LeaderboardPageStyles, TableStyles, TrStyles } from "./Style";
+// import { LeaderboardData } from "./data";
+
+export const LeaderboardPage = () => {
+  return (
+    <LeaderboardPageStyles>
+      <div className="cont">
+        <div className="head">
+          <CupIcon />
+          <h3>Leaderboard</h3>
+        </div>
+        <TableStyles>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Player</th>
+              <th>Moves</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {LeaderboardData.map((ele, index) => (
+              <TableRow
+                key={index}
+                player={ele.player}
+                moves={ele.moves}
+                time={ele.time}
+                index={index}
+              />
+            ))} */}
+          </tbody>
+        </TableStyles>
+      </div>
+    </LeaderboardPageStyles>
+  );
+};
+
+export interface ITableData {
+  player: string;
+  moves: number;
+  time: string;
+}
+interface ITableRow extends ITableData {
+  index: number;
+}
+export const TableRow: React.FC<ITableRow> = ({
+  player,
+  moves,
+  time,
+  index,
+}) => {
+  return (
+    <TrStyles>
+      <td>
+        <div className="circle">{index + 1}</div>
+      </td>
+      <td>
+        <div className="fl">
+          <UserIcon colorCode="#6BC683" />
+          <p>{player}</p>
+        </div>
+      </td>
+      <td>
+        <p>{moves}</p>
+      </td>
+      <td>
+        <p>{time}</p>
+      </td>
+    </TrStyles>
+  );
+};

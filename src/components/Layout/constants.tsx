286925e8af375090b1944e7afc42278c20dc28f3
Jude Tochy <tochyokoye@gmail.com>
2024-05-22 21:01:08 +0100

    reverting changes to main

--- src/components/Layout/constants.tsx
+++
@@ -1,9 +0,0 @@
-export interface IPageButton {
-    isActive: boolean;
-    name: string;
-  }
-
-export const PageButtons:IPageButton[] =[
-    {name : "Play Game", isActive : true},
-    {name : "Leaderboard", isActive : false},
-]
--- src/utils/validateEmail.ts
+++
@@ -1,6 +0,0 @@
-
-export const validateEmail = (email: string): boolean => {
-    // Regular expression for email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };

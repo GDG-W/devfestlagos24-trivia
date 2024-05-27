d0e24adf539997134e10f61a62b28f52cf75a011
Chukwuemeka Chukwurah <emekaemmanuel045@gmail.com>
2024-05-27 18:54:37 +0100

    feat: add duration and moves to leaderboard metadata

--- functions/src/attempts/attempt.repo.ts
+++ functions/src/attempts/attempt.repo.ts
@@ -12,10 +12,10 @@ class AttemptsRepository {
   async geRankings(): Promise<Attempt[]> {
     const results = await db.raw(`
     with scores as (
-      select users.name, users.email_address, users.id, attempts.created_at,attempts.score,
+      select users.name, users.email_address, users.id, attempts.created_at,attempts.score,moves,duration,
       row_number() over(partition by users.id order by score desc) as pos 
       from attempts inner join users on attempts.user_id=users.id
-    ), arrangements as (select id,name,email_address,score from scores where pos=1 order by score desc,created_at asc limit 50)
+    ), arrangements as (select id,name,email_address,moves,duration,score from scores where pos=1 order by score desc,created_at asc limit 50)
     select *, row_number() over() as position from arrangements
     `);
     return results.rows;
